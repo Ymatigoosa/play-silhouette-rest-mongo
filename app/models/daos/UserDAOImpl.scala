4af48c56c936a6b1c35e5f16c8350359be45aad3
Pahomov Dmitry <topt.iiiii@gmail.com>
2016-10-02 17:33:28 +0300

    move userDAO to reactive mongo

--- app/models/daos/UserDAOImpl.scala
+++ app/models/daos/UserDAOImpl.scala
@@ -4,15 +4,30 @@ import java.util.UUID
 
 import com.mohiva.play.silhouette.api.LoginInfo
 import models.User
-import models.daos.UserDAOImpl._
 
 import scala.collection.mutable
 import scala.concurrent.Future
+import javax.inject.Inject
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import reactivemongo.api._
+
+import play.modules.reactivemongo.{ // ReactiveMongo Play2 plugin
+  ReactiveMongoApi
+}
+
+// BSON-JSON conversions/collection
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection.JSONCollection
 
 /**
  * Give access to the user object.
  */
-class UserDAOImpl extends UserDAO {
+class UserDAOImpl @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends UserDAO {
+
+  def collection: Future[JSONCollection] =
+    reactiveMongoApi.database.map(_.collection[JSONCollection]("user"))
 
   /**
    * Finds a user by its login info.
@@ -20,8 +35,8 @@ class UserDAOImpl extends UserDAO {
    * @param loginInfo The login info of the user to find.
    * @return The found user or None if no user for the given login info could be found.
    */
-  def find(loginInfo: LoginInfo) = {
-    Future.successful(users.find { case (id, user) => user.loginInfo == loginInfo }.map(_._2))
+  def find(loginInfo: LoginInfo): Future[Option[User]] = {
+    collection.flatMap(_.find(Json.obj("loginInfo" -> loginInfo)).one[User])
   }
 
   /**
@@ -30,8 +45,8 @@ class UserDAOImpl extends UserDAO {
    * @param userID The ID of the user to find.
    * @return The found user or None if no user for the given ID could be found.
    */
-  def find(userID: UUID) = {
-    Future.successful(users.get(userID))
+  def find(userID: UUID): Future[Option[User]] = {
+    collection.flatMap(_.find(Json.obj("userID" -> userID)).one[User])
   }
 
   /**
@@ -41,18 +56,9 @@ class UserDAOImpl extends UserDAO {
    * @return The saved user.
    */
   def save(user: User) = {
-    users += (user.userID -> user)
-    Future.successful(user)
+    for {
+      c <- collection
+      _ <- c.insert(user)
+    } yield user
   }
-}
-
-/**
- * The companion object.
- */
-object UserDAOImpl {
-
-  /**
-   * The list of users.
-   */
-  val users: mutable.HashMap[UUID, User] = mutable.HashMap()
 }
